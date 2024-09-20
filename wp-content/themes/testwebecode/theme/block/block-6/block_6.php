8bfa86ec53d778d7bcb7627aefc40f70f378d46e
Auréliane Gagliardi <aurelianeg2000@gmail.com>
2024-09-20 15:01:40 +0200

    details

--- wp-content/themes/testwebecode/theme/block/block-6/block_6.php
+++ wp-content/themes/testwebecode/theme/block/block-6/block_6.php
@@ -25,7 +25,7 @@
             <?php elseif ($layout == 'text_with_image'): ?>
                       
                 <?php if ($style_1 == 'style_1'): ?>
-                    <div class="order-4 md:order-none col-start-1 md:col-end-5 lg:col-end-5 px-10 md:pb-12 md:pt-16 py-10 bg-white shadow-xl rounded-3xl relative z-10 md:left-[10px] md:top-[40px] <?php echo ($style_1 == 'style_2') ? 'text-center' : 'text-left'; ?>">
+                    <div class="order-4 md:order-none col-start-1 md:col-end-5 lg:col-end-5 px-7 md:pb-12 md:pt-16 py-10 bg-white shadow-xl rounded-3xl relative z-10 md:left-[10px] md:top-[40px] <?php echo ($style_1 == 'style_2') ? 'text-center' : 'text-left'; ?>">
                         <!-- Style 1: Title aligned left, image top-right -->
                         <div class="flex md:justify-between mb-3 md:mb-0 items-end md:items-start md:max-w-[75%] lg:max-w-full">
                             <?php if( $image ): ?>
@@ -39,7 +39,7 @@
                     </div>
 
                 <?php elseif ($style_1 == 'style_2'): ?>
-                    <div class="order-4 md:order-none col-start-1 md:col-end-5 px-10 md:pb-16 md:pt-16 py-10 bg-white shadow-xl rounded-3xl relative z-10 md:left-[10px] md:top-[50px] <?php echo ($style_1 == 'style_2') ? 'text-center' : 'text-left'; ?>">
+                    <div class="order-4 md:order-none col-start-1 md:col-end-5 px-7 md:pb-16 md:pt-16 py-16 bg-white shadow-xl rounded-3xl relative z-10 md:left-[10px] md:top-[50px] <?php echo ($style_1 == 'style_2') ? 'text-center' : 'text-left'; ?>">
                         <!-- Style 2: Title centered, image below, description hidden -->
                         <p class="font-text text-2xl font-bold mb-8"><?php echo $title; ?></p>
                         <?php if( $image ): ?>
@@ -49,7 +49,7 @@
                     </div>
 
                 <?php elseif ($style_1 == 'style_3'): ?>
-                    <div class="order-4 md:order-none col-start-1 md:col-end-5 px-10 md:pb-12 md:pt-16 py-10 bg-white shadow-xl rounded-3xl relative z-10 md:left-[10px] <?php echo ($style_1 == 'style_2') ? 'text-center' : 'text-left'; ?>">
+                    <div class="order-4 md:order-none col-start-1 md:col-end-5 px-7 md:pb-12 md:pt-16 py-10 bg-white shadow-xl rounded-3xl relative z-10 md:left-[10px] <?php echo ($style_1 == 'style_2') ? 'text-center' : 'text-left'; ?>">
                         <!-- Style 3: Only the image -->
                         <?php if( $image ): ?>
                             <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="object-cover mx-auto">
@@ -67,7 +67,7 @@
                 
             <!-- Block 2: Centered or Left-aligned Text with Title -->
             <?php elseif ($layout == 'centered_text'): ?>
-                <div class="order-3 md:order-none col-start-4 col-end-13 flex flex-col px-10 md:py-20 py-10 rounded-3xl shadow-xl md:relative z-20 md:top-[-80px] text-white bg-primary <?php echo ($style_2 == 'style_1') ? 'md:text-center md:items-center' : 'md:text-left md:items-start'; ?>">
+                <div class="order-3 md:order-none col-start-4 col-end-13 flex flex-col px-7 md:py-20 py-10 rounded-3xl shadow-xl md:relative z-20 md:top-[-80px] text-white bg-primary <?php echo ($style_2 == 'style_1') ? 'md:text-center md:items-center' : 'md:text-left md:items-start'; ?>">
                     <p class="font-text text-3xl font-semibold mb-8"><?php echo $title; ?></p>
                     <div class="font-text text-lg md:w-[75%] text-white prose leading-6">
                         <?php echo $text; ?>
@@ -76,14 +76,14 @@
 
             <!-- Block 3: Text Block (always left-aligned) -->
             <?php elseif ($layout == 'text_block'): ?>
-                <div class="order-5 md:order-none font-text text-xl col-start-1 col-end-7 px-10 md:px-6 pb-10 pt-14 bg-gradient-to-r from-[#058D8F] to-[#036855] text-white rounded-3xl md:relative md:top-[-98px] prose prose-strong:text-white">
+                <div class="order-5 md:order-none font-text text-xl col-start-1 col-end-7 px-7 md:px-6 pb-10 pt-14 bg-gradient-to-r from-[#058D8F] to-[#036855] text-white rounded-3xl md:relative md:top-[-98px] prose prose-strong:text-white">
                     <?php echo $text; ?>
                 </div>
 
             <!-- Block 4: Centered Text with Button -->
             <?php elseif ($layout == 'centered_text_with_button'): ?>
-                <div class="order-6 md:order-none col-start-8 col-end-12 px-6 pb-6 pt-8 text-center rounded-3xl border border-gray-200 md:relative z-10 md:bottom-[65px] md:h-[70%] flex flex-col justify-center items-center">
-                    <div class="font-text text-2xl mb-4 prose w-[90%]">
+                <div class="order-6 md:order-none col-start-8 col-end-12 px-7 md:px-5 pb-6 pt-8 text-center rounded-3xl border border-gray-200 md:relative z-10 md:bottom-[65px] md:h-[70%] flex flex-col justify-center items-center">
+                    <div class="font-text text-2xl mb-4 prose leading-7">
                         <?php echo $title; ?>
                     </div>
                     <?php
