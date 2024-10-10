fbdc4358006aa1bb9f8b9e5e1be2007364af6cea
Lameuhton <aurelianeg2000@gmail.com>
2024-10-10 12:03:02 +0200

    Refactor block 11 layout and styles for responsive design

--- wp-content/themes/testwebecode/theme/block/block-11/block_11.php
+++ wp-content/themes/testwebecode/theme/block/block-11/block_11.php
@@ -13,38 +13,61 @@ $tertiary_title_3 = get_field('tertiary_title_3'); // Titre tertiaire 3: champ "
 $description_tertiary_3 = get_field('description_tertiary_3'); // Description tertiaire 3: champ "zone de texte", nom "description_tertiary_3"
 $form_description = get_field('form_description'); // Description formulaire: champ "zone de texte", nom "form_description"
 ?>
-<section class="w-full relative mb-44">
+<section class="w-full mb-44">
     
-    
-
-    <div class="flex flex-col gap-5 md:gap-0 md:grid md:grid-cols-12 items-center relative z-10">
+    <div class="relative flex flex-col gap-5 md:gap-0 md:grid md:grid-cols-12 items-center z-10">
         <!-- Primary Title and Description -->
-        <div class="sides-page-margin relative col-span-12 bg-primary pb-28 text-white">
+        <div class="sides-page-margin col-span-12 bg-primary pb-28 text-white">
             <h1 class="h1 mb-3"><?php echo esc_html($main_title) ?></h1>
-            <p class="font-text text-[20px] leading-[24px] flex flex-col gap-7"><?php echo $description_main_title; ?></p>
+            <p class="font-text text-[20px] leading-[24px] flex flex-col gap-7 w-[40%]"><?php echo $description_main_title; ?></p>
         </div>
 
-        <div class="pl-5 lg:pl-36 flex flex-col col-start-1 col-end-5 order-2 md:order-none">
+        <div class="realtive pl-5 lg:pl-36 flex flex-col col-start-1 col-end-5 order-2 md:order-none">
             <h2 class="h2 mt-8 mb-5"><?php echo esc_html($secondary_title) ?></h2>
-            <p class="font-text text-[20px] leading-[24px] flex flex-col gap-7 mb-6"><?php echo $description_secondary_title; ?></p>
-            <div class="flex flex-col gap-1 mt-8">
-                <h3 class="font-text font-bold text-[24px]"><?php echo esc_html($tertiary_title_1) ?></h3>
-                <p class="paragraph ml-6"><?php echo $description_tertiary_1; ?></p>
+            <p class="font-text text-[20px] leading-[24px] flex flex-col gap-7 mb-16"><?php echo $description_secondary_title; ?></p>
+            
+            <div class="flex flex-row items-start gap-4 pb-5 relative left-[-10px]">
+                <!-- Cercle gauche -->
+                <div class="w-16 h-6 bg-primary rounded-full mt-1 relative z-10"></div>
+                <!-- Ligne verticale -->
+                <div class="absolute top-2 left-[11px] h-full border border-primary "></div>
+
+                <!-- Contenu -->
+                <div class="flex flex-col gap-1">
+                    <h3 class="font-text font-bold text-[24px]"><?php echo esc_html($tertiary_title_1) ?></h3>
+                    <p class="paragraph ml-6"><?php echo $description_tertiary_1; ?></p>
+                </div>
             </div>
-            <div class="flex flex-col gap-1 mt-8">
-                <h3 class="font-text font-bold text-[24px]"><?php echo esc_html($tertiary_title_2) ?></h3>
-                <p class="paragraph ml-6"><?php echo $description_tertiary_2; ?></p>
+
+            <div class="flex flex-row items-start gap-4 pb-5 relative left-[-10px]">
+                <!-- Cercle gauche -->
+                <div class="w-16 h-6 bg-primary rounded-full mt-1 relative z-10"></div>
+                <!-- Ligne verticale -->
+                <div class="absolute top-2 left-[11px] h-full border border-primary "></div>
+
+                <!-- Contenu -->
+                <div class="flex flex-col gap-1">
+                    <h3 class="font-text font-bold text-[24px]"><?php echo esc_html($tertiary_title_2) ?></h3>
+                    <p class="paragraph ml-6"><?php echo $description_tertiary_2; ?></p>
+                </div>
             </div>
-            <div class="flex flex-col gap-1 mt-8">
-                <h3 class="font-text font-bold text-[24px]"><?php echo esc_html($tertiary_title_3) ?></h3>
-                <p class="paragraph ml-6"><?php echo $description_tertiary_3; ?></p>
+
+            <div class="flex flex-row items-start gap-4 pb-5 relative left-[-10px]">
+                <!-- Cercle gauche -->
+                <div class="w-16 h-6 bg-primary rounded-full mt-1 relative z-10"></div>
+
+                <!-- Contenu -->
+                <div class="flex flex-col gap-1">
+                    <h3 class="font-text font-bold text-[24px]"><?php echo esc_html($tertiary_title_3) ?></h3>
+                    <p class="paragraph ml-6"><?php echo $description_tertiary_3; ?></p>
+                </div>
             </div>
         </div>
 
-        <div class="pr-5 lg:pr-36 order-1 md:order-none col-start-6 col-end-13 relative z-10">
-            <div class="bg-white gap-7 md:gap-10 px-10 sm:px-24 py-">
+        <div class="absolute pr-5 lg:pr-36 order-1 md:order-none col-start-6 col-end-13 z-10 bottom-[-20px]">
+            <div class="bg-white gap-7 md:gap-10 px-10 sm:px-24 py-28">
                 <div class="flex flex-col gap-1">
-                    <p class="paragraph"><?php echo $form_description; ?></p>
+                    <p class="font-text text-[24px] mb-10"><?php echo $form_description; ?></p>
                     <?php echo do_shortcode('[contact-form-7 id="60c8eb0" title="Contact form"]'); ?>
                 </div>
             </div>
--- wp-content/themes/testwebecode/theme/template-parts/layout/header-content.php
+++ wp-content/themes/testwebecode/theme/template-parts/layout/header-content.php
@@ -22,7 +22,7 @@ $logo_link = $is_contact_page ? 'http://template-main.test/wp-content/uploads/20
 $bg_class = $is_contact_page ? 'bg-primary' : '';
 ?>
 
-<header class="sides-page-margin w-full gap-14 flex items-baseline relative z-40 md:pt-12 md:pb-20 py-12<?php echo $bg_class ?>">
+<header class="sides-page-margin w-full gap-14 flex items-baseline relative z-40 md:pt-12 md:pb-20 py-12 <?php echo $bg_class ?>">
     <!-- Logo -->
     <div class="flex items-center">
         <a href="<?php echo home_url(); ?>">
